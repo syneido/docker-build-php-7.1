e7b3ec8976e8baaee20c7394facdd508097a6058
Michel MAIER <michel.maier@gmail.com>
2021-06-23 21:28:24 +0200

    feat(build): initial commit + github workflows

---
+++ configure.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env sh
+
+set -ex
+
+PHP_EXTENSIONS="opcache bcmath bz2 calendar exif gd gettext gmp intl mcrypt recode shmop soap sockets sysvmsg sysvsem sysvshm tidy xsl mysqli pdo_mysql wddx zip pcntl"
+PECL_EXTENSION_PACKAGES="apcu-5.1.20 imagick-3.4.4 sqlsrv-5.6.1 pdo_sqlsrv-5.6.1"
+PECL_EXTENSIONS="apcu imagick sqlsrv pdo_sqlsrv"
+RUN_DEPS="unzip libzip icu libxslt imagemagick libmcrypt unixodbc recode tidyhtml freetype libjpeg-turbo libpng libwebp libxpm"
+BUILD_DEPS="autoconf g++ make libzip-dev zlib-dev libpng-dev libxml2-dev icu-dev unixodbc-dev bzip2-dev libc-dev gmp-dev libmcrypt-dev recode-dev gettext-dev tidyhtml-dev libxslt-dev imagemagick-dev freetype-dev libjpeg-turbo-dev libpng-dev libwebp-dev libxpm-dev tzdata"
+
+apk add --no-cache --virtual rundeps ${RUN_DEPS}
+apk add --no-cache --virtual .build-deps ${BUILD_DEPS}
+
+### Fix pecl for official php7.1 image
+pecl channel-update pecl.php.net
+
+docker-php-source extract
+docker-php-ext-configure gd --with-gd --with-freetype-dir=/usr/include/ --with-jpeg-dir=/usr/include/ --with-png-dir=/usr/include/ --with-xpm-dir=/usr/include/ --with-webp-dir=/usr/include/
+docker-php-ext-install -j"$(nproc)" ${PHP_EXTENSIONS}
+pecl install ${PECL_EXTENSION_PACKAGES}
+docker-php-ext-enable ${PECL_EXTENSIONS}
+
+docker-php-source delete
+rm -r /tmp/pear/cache/* /tmp/pear/download/*
+
+### TimeZone
+cp /usr/share/zoneinfo/Europe/Paris /etc/localtime
+echo "Europe/Paris" >  /etc/timezone
+
+apk del .build-deps
