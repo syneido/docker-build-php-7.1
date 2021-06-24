4a710f2da28998a1b1b284584c07f18050c66571
Michel MAIER <michel.maier@gmail.com>
2021-06-24 14:50:21 +0200

    feat(RUN_DEPS): remove useless git

--- configure.sh
+++ configure.sh
@@ -5,7 +5,7 @@ set -ex
 PHP_EXTENSIONS="opcache bcmath bz2 calendar exif gd gettext gmp intl mcrypt recode shmop soap sockets sysvmsg sysvsem sysvshm tidy xsl mysqli pdo_mysql wddx zip pcntl"
 PECL_EXTENSION_PACKAGES="apcu-5.1.20 imagick-3.4.4 sqlsrv-5.6.1 pdo_sqlsrv-5.6.1"
 PECL_EXTENSIONS="apcu imagick sqlsrv pdo_sqlsrv"
-RUN_DEPS="git unzip libzip icu libxslt imagemagick libmcrypt unixodbc recode tidyhtml freetype libjpeg-turbo libpng libwebp libxpm"
+RUN_DEPS="unzip libzip icu libxslt imagemagick libmcrypt unixodbc recode tidyhtml freetype libjpeg-turbo libpng libwebp libxpm"
 BUILD_DEPS="autoconf g++ make libzip-dev zlib-dev libpng-dev libxml2-dev icu-dev unixodbc-dev bzip2-dev libc-dev gmp-dev libmcrypt-dev recode-dev gettext-dev tidyhtml-dev libxslt-dev imagemagick-dev freetype-dev libjpeg-turbo-dev libpng-dev libwebp-dev libxpm-dev tzdata"
 
 apk add --no-cache --virtual rundeps ${RUN_DEPS}
